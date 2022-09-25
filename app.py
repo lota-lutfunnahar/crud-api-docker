6511cf78f567b47fc40efc65be4d852349818458
Lutfunnahar Lota <l.nahar@ppln.co>
2022-09-25 13:12:48 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80), unique=True, nullable=False)
+  content = db.Column(db.String(120), unique=True, nullable=False)
+
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
+
+
+db.create_all()
+
+
+@app.route('/items/<id>', methods=['GET'])
+def get_item(id):
+  item = Item.query.get(id)
+  del item.__dict__['_sa_instance_state']
+  return jsonify(item.__dict__)
+
+@app.route('/items', methods=['GET'])
+def get_items():
+  items = []
+  for item in db.session.query(Item).all():
+    del item.__dict__['_sa_instance_state']
+    items.append(item.__dict__)
+  return jsonify(items)
+
+@app.route('/items', methods=['POST'])
+def create_item():
+  body = request.get_json()
+  db.session.add(Item(body['title'], body['content']))
+  db.session.commit()
+  return "item created"
+
+@app.route('/items/<id>', methods=['PUT'])
+def update_item(id):
+  body = request.get_json()
+  db.session.query(Item).filter_by(id=id).update(
+    dict(title=body['title'], content=body['content']))
+  db.session.commit()
+  return "item updated"
+
+@app.route('/items/<id>', methods=['DELETE'])
+def delete_item(id):
+  db.session.query(Item).filter_by(id=id).delete()
+  db.session.commit()
+  return "item deleted"
